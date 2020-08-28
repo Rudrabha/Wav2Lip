35d96b32814cb9684e0af64b901f9f114f8a5f24
Prajwal <prajwalrenukanand@gmail.com>
2020-08-28 21:14:00 +0530

    Reduce sync loss weight

--- wav2lip_train.py
+++ wav2lip_train.py
@@ -251,7 +251,7 @@ def train(device, model, train_data_loader, test_data_loader, optimizer,
                     average_sync_loss = eval_model(test_data_loader, global_step, device, model, checkpoint_dir)
 
                     if average_sync_loss < .75:
-                        hparams.set_hparam('syncnet_wt', 0.03)
+                        hparams.set_hparam('syncnet_wt', 0.01) # without image GAN a lesser weight is sufficient
 
             prog_bar.set_description('L1: {}, Sync Loss: {}'.format(running_l1_loss / (step + 1),
                                                                     running_sync_loss / (step + 1)))

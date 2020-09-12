0600d0f4da5ce75865725249fdcb88b6dc2d61de
prajwalkr <prajwalrenukanand@gmail.com>
2020-09-12 09:53:46 +0530

    Fix: Generate last frame

--- inference.py
+++ inference.py
@@ -231,6 +231,7 @@ def main():
 	while 1:
 		start_idx = int(i * mel_idx_multiplier)
 		if start_idx + mel_step_size > len(mel[0]):
+			mel_chunks.append(mel[:, len(mel[0]) - mel_step_size:])
 			break
 		mel_chunks.append(mel[:, start_idx : start_idx + mel_step_size])
 		i += 1

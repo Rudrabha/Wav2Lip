07b5fae35121c9c5e9308c06c48da5d8fe5fd44b
rudrabha <rudrabha@gmail.com>
2020-09-24 13:00:35 +0530

    Added scripts for lrs videos

--- evaluation/scores/scores_calc_real_videos.sh
+++
@@ -1,6 +0,0 @@
-yourfilenames=`ls $1`
-for eachfile in $yourfilenames
-do
-   python run_pipeline.py --videofile $eachfile --reference lipsyncnet --data_dir /ssd_scratch/cvit/prajwalkr_rudra/tmp_dir
-   python calc_real_video.py --videofile $eachfile --reference lipsyncnet --data_dir /ssd_scratch/cvit/prajwalkr_rudra/tmp_dir >> test_score.txt
-done

fda2a15d1437d8a66cdf978e4bedb17652a6276c
Prajwal <prajwalrenukanand@gmail.com>
2020-10-26 15:17:30 +0530

    Bugfix
    Credit: https://github.com/Rudrabha/Wav2Lip/issues/109#issuecomment-716413308

--- evaluation/scores_LSE/calculate_scores_real_videos.sh
+++ evaluation/scores_LSE/calculate_scores_real_videos.sh
@@ -1,7 +1,8 @@
 rm all_scores.txt
 yourfilenames=`ls $1`
+
 for eachfile in $yourfilenames
 do
-   python run_pipeline.py --videofile $eachfile --reference wav2lip --data_dir tmp_dir
-   python calculate_scores_real_videos.py --videofile $eachfile --reference wav2lip --data_dir /ssd_scratch/cvit/prajwalkr_rudra/tmp_dir >> all_scores.txt
+   python run_pipeline.py --videofile $1/$eachfile --reference wav2lip --data_dir tmp_dir
+   python calculate_scores_real_videos.py --videofile $1/$eachfile --reference wav2lip --data_dir tmp_dir >> all_scores.txt
 done

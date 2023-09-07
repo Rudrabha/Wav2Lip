eb93fb0ef51189b54ff15884bf8cbf1e49e620d7
Justin John <34035011+justinjohn0306@users.noreply.github.com>
2023-09-07 21:09:00 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -38,8 +38,8 @@ parser.add_argument('--pads', nargs='+', type=int, default=[0, 10, 0, 0],
 
 parser.add_argument('--wav2lip_batch_size', type=int, help='Batch size for Wav2Lip model(s)', default=128)
 
-# parser.add_argument('--resize_factor', default=1, type=int,
-#             help='Reduce the resolution by this factor. Sometimes, best results are obtained at 480p or 720p')
+parser.add_argument('--resize_factor', default=1, type=int,
+             help='Reduce the resolution by this factor. Sometimes, best results are obtained at 480p or 720p')
 
 parser.add_argument('--out_height', default=480, type=int,
             help='Output video height. Best results are obtained at 480 or 720')

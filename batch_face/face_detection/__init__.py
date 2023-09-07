367e809e323a1f805e3e43bc0a5af44aa73bc8bb
Justin John <34035011+justinjohn0306@users.noreply.github.com>
2023-09-07 16:58:52 +0530

    Delete batch_face directory

--- batch_face/face_detection/__init__.py
+++
@@ -1,2 +0,0 @@
-from ._version import __version__
-from .detector import RetinaFace
--- batch_face/face_reconstruction/__init__.py
+++
@@ -1,3 +0,0 @@
-from .utils import *
-from .regressor import ShapeRegressor
-from .utils import parse_param, predict_dense, predict_68pts

f2cbad0b999d32a0bfdd02c2104309f4b01ba5aa
Samuel Courthial <samuel.courthial.18@gmail.com>
2023-11-22 09:15:01 +0100

    Adapt inference code for v1

--- inference.py
+++ inference.py
@@ -1,4 +1,4 @@
-from os import listdir, path
+from os import listdir, path, makedirs
 import numpy as np
 import scipy, cv2, os, sys, argparse, audio
 import json, subprocess, random, string
@@ -7,39 +7,42 @@ from glob import glob
 import torch, face_detection
 from models import Wav2Lip
 import platform
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import mediapipe as mp
 
 parser = argparse.ArgumentParser(description='Inference code to lip-sync videos in the wild using Wav2Lip models')
 
-parser.add_argument('--checkpoint_path', type=str, 
+parser.add_argument('--checkpoint_path', type=str,
 					help='Name of saved checkpoint to load weights from', required=True)
 
-parser.add_argument('--face', type=str, 
+parser.add_argument('--face', type=str,
 					help='Filepath of video/image that contains faces to use', required=True)
-parser.add_argument('--audio', type=str, 
+parser.add_argument('--audio', type=str,
 					help='Filepath of video/audio file to use as raw audio source', required=True)
-parser.add_argument('--outfile', type=str, help='Video path to save result. See default for an e.g.', 
-								default='results/result_voice.mp4')
+parser.add_argument('--outfolder', type=str, help='Folder path to save result. See default for an e.g.',
+								default='results')
 
-parser.add_argument('--static', type=bool, 
+parser.add_argument('--static', type=bool,
 					help='If True, then use only first video frame for inference', default=False)
-parser.add_argument('--fps', type=float, help='Can be specified only if input is a static image (default: 25)', 
+parser.add_argument('--fps', type=float, help='Can be specified only if input is a static image (default: 25)',
 					default=25., required=False)
 
-parser.add_argument('--pads', nargs='+', type=int, default=[0, 10, 0, 0], 
+parser.add_argument('--pads', nargs='+', type=int, default=[0, 10, 0, 0],
 					help='Padding (top, bottom, left, right). Please adjust to include chin at least')
 
-parser.add_argument('--face_det_batch_size', type=int, 
+parser.add_argument('--face_det_batch_size', type=int,
 					help='Batch size for face detection', default=16)
 parser.add_argument('--wav2lip_batch_size', type=int, help='Batch size for Wav2Lip model(s)', default=128)
 
-parser.add_argument('--resize_factor', default=1, type=int, 
+parser.add_argument('--resize_factor', default=1, type=int,
 			help='Reduce the resolution by this factor. Sometimes, best results are obtained at 480p or 720p')
 
-parser.add_argument('--crop', nargs='+', type=int, default=[0, -1, 0, -1], 
-					help='Crop video to a smaller region (top, bottom, left, right). Applied after resize_factor and rotate arg. ' 
+parser.add_argument('--crop', nargs='+', type=int, default=[0, -1, 0, -1],
+					help='Crop video to a smaller region (top, bottom, left, right). Applied after resize_factor and rotate arg. '
 					'Useful if multiple face present. -1 implies the value will be auto-inferred based on height, width')
 
-parser.add_argument('--box', nargs='+', type=int, default=[-1, -1, -1, -1], 
+parser.add_argument('--box', nargs='+', type=int, default=[-1, -1, -1, -1],
 					help='Specify a constant bounding box for the face. Use only as a last resort if the face is not detected.'
 					'Also, might work only if the face is not moving around much. Syntax: (top, bottom, left, right).')
 
@@ -50,6 +53,12 @@ parser.add_argument('--rotate', default=False, action='store_true',
 parser.add_argument('--nosmooth', default=False, action='store_true',
 					help='Prevent smoothing face detections over a short temporal window')
 
+parser.add_argument('--face_landmarks_detector_path', default='weights/face_landmarker_v2_with_blendshapes.task', type=str,
+					help='Path to face landmarks detector')
+
+parser.add_argument('--with_face_mask', default=True, action='store_true',
+					help='Blend output into original frame using a face mask rather than directly blending the face box. This prevents a lower resolution square artifact around lower face')
+
 args = parser.parse_args()
 args.img_size = 96
 
@@ -57,8 +66,20 @@ if os.path.isfile(args.face) and args.face.split('.')[1] in ['jpg', 'png', 'jpeg
 	args.static = True
 
 def get_smoothened_boxes(boxes, T):
+	empty_indexes = []
+	for idx, box in enumerate(boxes):
+		if not box.any(): empty_indexes.append(idx)
+
+	empty_indexes = iter(empty_indexes)
+	next_empty_index = next(empty_indexes, None)
 	for i in range(len(boxes)):
-		if i + T > len(boxes):
+		if i == next_empty_index:
+			next_empty_index = next(empty_indexes, None)
+			continue
+
+		if next_empty_index is not None and i + T > next_empty_index:
+			window = boxes[i : next_empty_index]
+		elif i + T > len(boxes):
 			window = boxes[len(boxes) - T:]
 		else:
 			window = boxes[i : i + T]
@@ -66,18 +87,18 @@ def get_smoothened_boxes(boxes, T):
 	return boxes
 
 def face_detect(images):
-	detector = face_detection.FaceAlignment(face_detection.LandmarksType._2D, 
+	detector = face_detection.FaceAlignment(face_detection.LandmarksType._2D,
 											flip_input=False, device=device)
 
 	batch_size = args.face_det_batch_size
-	
+
 	while 1:
 		predictions = []
 		try:
 			for i in tqdm(range(0, len(images), batch_size)):
 				predictions.extend(detector.get_detections_for_batch(np.array(images[i:i + batch_size])))
 		except RuntimeError:
-			if batch_size == 1: 
+			if batch_size == 1:
 				raise RuntimeError('Image too big to run face detection on GPU. Please use the --resize_factor argument')
 			batch_size //= 2
 			print('Recovering from OOM error; New batch size: {}'.format(batch_size))
@@ -88,22 +109,21 @@ def face_detect(images):
 	pady1, pady2, padx1, padx2 = args.pads
 	for rect, image in zip(predictions, images):
 		if rect is None:
-			cv2.imwrite('temp/faulty_frame.jpg', image) # check this frame where the face was not detected.
-			raise ValueError('Face not detected! Ensure the video contains a face in all the frames.')
+			results.append([])
+		else:
+			y1 = max(0, rect[1] - pady1)
+			y2 = min(image.shape[0], rect[3] + pady2)
+			x1 = max(0, rect[0] - padx1)
+			x2 = min(image.shape[1], rect[2] + padx2)
 
-		y1 = max(0, rect[1] - pady1)
-		y2 = min(image.shape[0], rect[3] + pady2)
-		x1 = max(0, rect[0] - padx1)
-		x2 = min(image.shape[1], rect[2] + padx2)
-		
-		results.append([x1, y1, x2, y2])
+			results.append([x1, y1, x2, y2])
 
 	boxes = np.array(results)
 	if not args.nosmooth: boxes = get_smoothened_boxes(boxes, T=5)
-	results = [[image[y1: y2, x1:x2], (y1, y2, x1, x2)] for image, (x1, y1, x2, y2) in zip(images, boxes)]
+	results = [([image[y1: y2, x1:x2], (y1, y2, x1, x2)] if x1 is not None and y1 is not None and x2 is not None and y2 is not None else [[],[]]) for image, (x1, y1, x2, y2) in zip(images, boxes)]
 
 	del detector
-	return results 
+	return results
 
 def datagen(frames, mels):
 	img_batch, mel_batch, frame_batch, coords_batch = [], [], [], []
@@ -123,12 +143,16 @@ def datagen(frames, mels):
 		frame_to_save = frames[idx].copy()
 		face, coords = face_det_results[idx].copy()
 
-		face = cv2.resize(face, (args.img_size, args.img_size))
-			
+		if len(coords) == 0:
+			face = np.random.rand(args.img_size, args.img_size, 3) * 255
+			coords_batch.append([])
+		else:
+			face = cv2.resize(face, (args.img_size, args.img_size))
+			coords_batch.append(coords)
+
 		img_batch.append(face)
 		mel_batch.append(m)
 		frame_batch.append(frame_to_save)
-		coords_batch.append(coords)
 
 		if len(img_batch) >= args.wav2lip_batch_size:
 			img_batch, mel_batch = np.asarray(img_batch), np.asarray(mel_batch)
@@ -177,6 +201,37 @@ def load_model(path):
 
 	model = model.to(device)
 	return model.eval()
+
+def face_mask_from_image(image, face_landmarks_detector):
+	"""
+	Calculate face mask from image. This is done by
+
+	Args:
+		image: numpy array of an image
+		face_landmarks_detector: mediapipa face landmarks detector
+	Returns:
+		A uint8 numpy array with the same height and width of the input image, containing a binary mask of the face in the image
+	"""
+	# initialize mask
+	mask = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint8)
+
+	# detect face landmarks
+	mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+	detection = face_landmarks_detector.detect(mp_image)
+
+	if len(detection.face_landmarks) == 0:
+		# no face detected - set mask to all of the image
+		mask[:] = 1
+		return mask
+
+	# extract landmarks coordinates
+	face_coords = np.array([[lm.x * image.shape[1], lm.y * image.shape[0]] for lm in detection.face_landmarks[0]])
+
+	# calculate convex hull from face coordinates
+	convex_hull = cv2.convexHull(face_coords.astype(np.float32))
+
+	# apply convex hull to mask
+	return cv2.fillPoly(mask, pts=[convex_hull.squeeze().astype(np.int32)], color=1)
 
 def main():
 	if not os.path.isfile(args.face):
@@ -229,7 +284,7 @@ def main():
 		raise ValueError('Mel contains nan! Using a TTS voice? Add a small epsilon noise to the wav file and try again')
 
 	mel_chunks = []
-	mel_idx_multiplier = 80./fps 
+	mel_idx_multiplier = 80./fps
 	i = 0
 	while 1:
 		start_idx = int(i * mel_idx_multiplier)
@@ -246,15 +301,24 @@ def main():
 	batch_size = args.wav2lip_batch_size
 	gen = datagen(full_frames.copy(), mel_chunks)
 
-	for i, (img_batch, mel_batch, frames, coords) in enumerate(tqdm(gen, 
+	face_landmarks_detector = None
+	if args.with_face_mask and args.face_landmarks_detector_path:
+		base_options = python.BaseOptions(model_asset_path=args.face_landmarks_detector_path)
+		options = vision.FaceLandmarkerOptions(
+			base_options=base_options,
+			output_face_blendshapes=True,
+			output_facial_transformation_matrixes=True,
+			num_faces=1
+		)
+		face_landmarks_detector = vision.FaceLandmarker.create_from_options(options)
+
+	os.makedirs(args.outfolder, exist_ok=True)
+
+	for i, (img_batch, mel_batch, frames, coords) in enumerate(tqdm(gen,
 											total=int(np.ceil(float(len(mel_chunks))/batch_size)))):
 		if i == 0:
 			model = load_model(args.checkpoint_path)
 			print ("Model loaded")
-
-			frame_h, frame_w = full_frames[0].shape[:-1]
-			out = cv2.VideoWriter('temp/result.avi', 
-									cv2.VideoWriter_fourcc(*'DIVX'), fps, (frame_w, frame_h))
 
 		img_batch = torch.FloatTensor(np.transpose(img_batch, (0, 3, 1, 2))).to(device)
 		mel_batch = torch.FloatTensor(np.transpose(mel_batch, (0, 3, 1, 2))).to(device)
@@ -263,18 +327,19 @@ def main():
 			pred = model(mel_batch, img_batch)
 
 		pred = pred.cpu().numpy().transpose(0, 2, 3, 1) * 255.
-		
-		for p, f, c in zip(pred, frames, coords):
+
+		for j, (p, f, c) in enumerate(zip(pred, frames, coords)):
 			y1, y2, x1, x2 = c
-			p = cv2.resize(p.astype(np.uint8), (x2 - x1, y2 - y1))
 
-			f[y1:y2, x1:x2] = p
-			out.write(f)
+			if len(c) > 0:
+				p = cv2.resize(p.astype(np.uint8), (x2 - x1, y2 - y1))
+				if face_landmarks_detector:
+					mask = face_mask_from_image(p, face_landmarks_detector)
+					f[y1:y2, x1:x2] = f[y1:y2, x1:x2] * (1 - mask[..., None]) + p * mask[..., None]
+				else:
+					f[y1:y2, x1:x2] = p
 
-	out.release()
-
-	command = 'ffmpeg -y -i {} -i {} -strict -2 -q:v 1 {}'.format(args.audio, 'temp/result.avi', args.outfile)
-	subprocess.call(command, shell=platform.system() != 'Windows')
+			cv2.imwrite(f'{args.outfolder}/{i * batch_size + j:06d}.png', f)
 
 if __name__ == '__main__':
 	main()

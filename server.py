528734662b65ee3fa0538fa22e4ebec4fae42885
Sammy Fischer <sammy@cosmic-bandito.com>
2024-01-01 16:51:44 +0100

    Refactor argparse to class-based argument structure
    Argparse command-line structure was refactored into a class-based Args() attribute initialization. This approach converted all previous command-line arguments into class attributes, maintaining their default values, and made arguments more accessible, easier to manage and change. Arguments are now contained within the 'Args' object.

--- server.py
+++ server.py
@@ -16,50 +16,25 @@ import platform
 app = Flask(__name__)
 
 
-parser = argparse.ArgumentParser(description='Inference code to lip-sync videos in the wild using Wav2Lip models')
+class Args:
+    def __init__(self):
+        self.checkpoint_path = "checkpoints/wav2lip.pth"
+        self.face = ""
+        self.audio = "audio"
+        self.outfile = 'results/result_voice.mp4'
+        self.static = False
+        self.fps = 25.
+        self.pads = [0, 10, 0, 0]
+        self.face_det_batch_size = 16
+        self.wav2lip_batch_size = 128
+        self.resize_factor = 1
+        self.crop = [0, -1, 0, -1]
+        self.box = [-1, -1, -1, -1]
+        self.rotate = False
+        self.nosmooth = False
+        self.img_size = 96
 
-parser.add_argument('--checkpoint_path', type=str,
-                    help='Name of saved checkpoint to load weights from', required=False, default="checkpoints/wav2lip.pth")
-
-parser.add_argument('--face', type=str,
-                    help='Filepath of video/image that contains faces to use', required=False, default="")
-parser.add_argument('--audio', type=str,
-                    help='Filepath of video/audio file to use as raw audio source', required=False, default="audio")
-parser.add_argument('--outfile', type=str, help='Video path to save result. See default for an e.g.',
-                    default='results/result_voice.mp4')
-
-parser.add_argument('--static', type=bool,
-                    help='If True, then use only first video frame for inference', default=False)
-parser.add_argument('--fps', type=float, help='Can be specified only if input is a static image (default: 25)',
-                    default=25., required=False)
-
-parser.add_argument('--pads', nargs='+', type=int, default=[0, 10, 0, 0],
-                    help='Padding (top, bottom, left, right). Please adjust to include chin at least')
-
-parser.add_argument('--face_det_batch_size', type=int,
-                    help='Batch size for face detection', default=16)
-parser.add_argument('--wav2lip_batch_size', type=int, help='Batch size for Wav2Lip model(s)', default=128)
-
-parser.add_argument('--resize_factor', default=1, type=int,
-                    help='Reduce the resolution by this factor. Sometimes, best results are obtained at 480p or 720p')
-
-parser.add_argument('--crop', nargs='+', type=int, default=[0, -1, 0, -1],
-                    help='Crop video to a smaller region (top, bottom, left, right). Applied after resize_factor and rotate arg. '
-                         'Useful if multiple face present. -1 implies the value will be auto-inferred based on height, width')
-
-parser.add_argument('--box', nargs='+', type=int, default=[-1, -1, -1, -1],
-                    help='Specify a constant bounding box for the face. Use only as a last resort if the face is not detected.'
-                         'Also, might work only if the face is not moving around much. Syntax: (top, bottom, left, right).')
-
-parser.add_argument('--rotate', default=False, action='store_true',
-                    help='Sometimes videos taken from a phone can be flipped 90deg. If true, will flip video right by 90deg.'
-                         'Use if you get a flipped result, despite feeding a normal looking video')
-
-parser.add_argument('--nosmooth', default=False, action='store_true',
-                    help='Prevent smoothing face detections over a short temporal window')
-
-args = parser.parse_args()
-args.img_size = 96
+args = Args()
 
 global model
 model_path ="checkpoints/wav2lip.pth"

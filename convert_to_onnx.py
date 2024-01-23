ad83ffbfc162a71a8e1007459ff48439d27caa2f
Łukasz Kita <lukasz.kita0@gmail.com>
2024-01-23 15:42:03 +0100

    Update convert_to_onnx script

--- convert_to_onnx.py
+++ convert_to_onnx.py
@@ -1,28 +1,38 @@
 import torch
 import torch.onnx
-from models import Wav2Lip
+from models.wav2lip import Wav2Lip
+from sys import argv
+
+model_name = "wav2lip"
 
 model = Wav2Lip()
-checkpoint_path = "checkpoints/wav2lip.pth"
-modelz = torch.load(checkpoint_path, map_location=torch.device('cpu'))
 
-device="cpu"
-s = modelz["state_dict"]
-new_s = {}
-for k, v in s.items():
-    new_s[k.replace('module.', '')] = v
+state = torch.load(f"checkpoints/{model_name}.pth", map_location=lambda storage, loc: storage)
+cleaned_state = { key.replace('module.', ''): value for (key, value) in state['state_dict'].items() }
 
-model.load_state_dict(new_s)
-model = model.to(device)
-
+model.load_state_dict(cleaned_state)
 model.eval()
-input_shape1 = (8, 1, 80, 16)
-input_shape2 = (8, 6, 96, 96)
 
-torch.onnx.export(model,
-                  (torch.randn(*input_shape1, device=device), torch.randn(*input_shape2, device=device)),
-                  "wav2lip.onnx",
-                  input_names=["mel_spectrogram", "video_frames"],
-                  output_names=["predicted_frames"],
-                  verbose=True,
-                  opset_version=18)
+# 16 samples (~200ms audio) of 80-mel spectrum data, transposed (0, 3, 1, 2)
+# Autograd needed for...some reason? Maybe constant folding?
+mel_batch = torch.randn(1, 1, 80, 16, requires_grad=True)
+
+# frame of 96x96 video centered on an upright face in BGR format, concatenated
+# on channel (innermost) axis with the same face but with the bottom half
+# zeroed out (i.e. 6 total color channels), then transposed (0, 3, 1, 2)
+img_batch = torch.randn(1, 6, 96, 96, requires_grad=True)
+
+torch.onnx.export(
+    model,
+    (mel_batch, img_batch),
+    f"{model_name}.onnx",
+    export_params=True,
+    do_constant_folding=True,
+    input_names=['mel', 'vid'],
+    output_names=['gen'],
+    dynamic_axes={
+        'mel': {0: 'batch'},
+        'vid': {0: 'batch'},
+        'gen': {0: 'batch'}
+    }
+)

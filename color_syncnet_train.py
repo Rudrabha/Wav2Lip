784fced2a475a9be6377697658d9642cc8c8968d
Eddy Ma <ocean_ltd@hotmail.com>
2024-03-31 04:33:53 -0700

    drop last for dataloader

--- color_syncnet_train.py
+++ color_syncnet_train.py
@@ -273,11 +273,11 @@ if __name__ == "__main__":
 
     train_data_loader = data_utils.DataLoader(
         train_dataset, batch_size=hparams.syncnet_batch_size, shuffle=True,
-        num_workers=hparams.num_workers)
+        num_workers=hparams.num_workers, drop_last=True)
 
     test_data_loader = data_utils.DataLoader(
         test_dataset, batch_size=hparams.syncnet_batch_size,
-        num_workers=8)
+        num_workers=8, drop_last=True)
 
     device = torch.device("cuda" if use_cuda else "cpu")
 

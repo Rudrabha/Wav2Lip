afb60c92d31481aebc81e084c2f46d8b6c8bca60
Eddy Ma <ocean_ltd@hotmail.com>
2024-04-02 01:11:00 -0700

    change img_size

--- hparams.py
+++ hparams.py
@@ -73,7 +73,7 @@ hparams = HParams(
 	fmax=7600,  # To be increased/reduced depending on data.
 
 	###################### Our training parameters #################################
-	img_size=96,
+	img_size=192,
 	fps=25,
 	
 	batch_size=16,

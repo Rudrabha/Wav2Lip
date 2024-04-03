8b0d5e5efd4db13bde0f84caf20d43bfc8479960
Eddy Ma <ocean_ltd@hotmail.com>
2024-04-02 19:15:50 -0700

    change syncnet interval

--- hparams.py
+++ hparams.py
@@ -87,8 +87,8 @@ hparams = HParams(
     syncnet_wt=0.0, # is initially zero, will be set automatically to 0.03 later. Leads to faster convergence. 
 	syncnet_batch_size=64,
 	syncnet_lr=1e-4,
-	syncnet_eval_interval=3000,
-	syncnet_checkpoint_interval=3000,
+	syncnet_eval_interval=1000,
+	syncnet_checkpoint_interval=1000,
 
 	disc_wt=0.07,
 	disc_initial_learning_rate=1e-4,

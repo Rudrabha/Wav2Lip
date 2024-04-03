4dadc2e33714dfd6361ff0ccc06f6d713760fbcf
Eddy Ma <ocean_ltd@hotmail.com>
2024-04-02 17:13:31 -0700

    change checkpoint interval

--- hparams.py
+++ hparams.py
@@ -80,15 +80,15 @@ hparams = HParams(
 	initial_learning_rate=1e-4,
 	nepochs=200000000000000000,  ### ctrl + c, stop whenever eval loss is consistently greater than train loss for ~10 epochs
 	num_workers=16,
-	checkpoint_interval=3000,
-	eval_interval=3000,
+	checkpoint_interval=1000,
+	eval_interval=1000,
     save_optimizer_state=True,
 
     syncnet_wt=0.0, # is initially zero, will be set automatically to 0.03 later. Leads to faster convergence. 
 	syncnet_batch_size=64,
 	syncnet_lr=1e-4,
-	syncnet_eval_interval=10000,
-	syncnet_checkpoint_interval=10000,
+	syncnet_eval_interval=3000,
+	syncnet_checkpoint_interval=3000,
 
 	disc_wt=0.07,
 	disc_initial_learning_rate=1e-4,

de9d00c361a39bc1fa2ed98181d4c0643f1ef64e
Eddy Ma <ocean_ltd@hotmail.com>
2024-04-16 15:48:02 -0700

    format printing

--- color_syncnet_train.py
+++ color_syncnet_train.py
@@ -212,6 +212,7 @@ def get_current_lr(optimizer):
 def eval_model(test_data_loader, global_step, device, model, checkpoint_dir, scheduler):
     #eval_steps = 1400
     eval_steps = 100
+    print()
     print('Evaluating for {0} steps of total steps {1}'.format(eval_steps, len(test_data_loader)))
     losses = []
     while 1:

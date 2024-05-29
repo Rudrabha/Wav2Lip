61f9bac60b4a431b3440f77aebe6fd8bbc85b11b
Eddy Ma <ocean_ltd@hotmail.com>
2024-05-28 23:39:46 -0700

    simplify the syncnet

--- models/syncnet.py
+++ models/syncnet.py
@@ -10,35 +10,29 @@ class SyncNet_color(nn.Module):
 
         self.face_encoder = nn.Sequential(
             # Added by eddy
-            Conv2d(15, 32, kernel_size=(7, 7), stride=1, padding=3), #192x192
-            Conv2d(32, 32, kernel_size=3, stride=1, padding=1), 
+            Conv2d(15, 64, kernel_size=(7, 7), stride=1, padding=3), #192x192
+            
             # End added
-            Conv2d(32, 32, kernel_size=3, stride=2, padding=1), #96x96
-            Conv2d(32, 32, kernel_size=(7, 7), stride=1, padding=3), 
-            Conv2d(32, 32, kernel_size=(7, 7), stride=1, padding=3, residual=True),
-            Conv2d(32, 32, kernel_size=(7, 7), stride=1, padding=3, residual=True),
-            nn.Dropout(0.4),
+            Conv2d(64, 64, kernel_size=3, stride=2, padding=1), #96x96
+            Conv2d(64, 64, kernel_size=(7, 7), stride=1, padding=3, residual=True),
+            Conv2d(64, 64, kernel_size=(7, 7), stride=1, padding=3, residual=True),
 
-            Conv2d(32, 64, kernel_size=5, stride=(1, 2), padding=1), #94x47
+            Conv2d(64, 64, kernel_size=5, stride=(1, 2), padding=1), #94x47
             Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True), #94x47
             Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True),#94x47
-            nn.Dropout(0.2),
 
             Conv2d(64, 128, kernel_size=3, stride=2, padding=1), # 47x24
             Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True), # 47x24
             Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True), # 47x24
             Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True), # 47x24
-            nn.Dropout(0.2),
 
             Conv2d(128, 256, kernel_size=3, stride=2, padding=1), # 24x 12
             Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True), # 24x 12
             Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True), # 24x 12
-            nn.Dropout(0.1),
 
             Conv2d(256, 512, kernel_size=3, stride=2, padding=1), #12x6
             Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True), #12x6
             Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True), #12x6
-            nn.Dropout(0.1),
 
             Conv2d(512, 512, kernel_size=3, stride=2, padding=1), #6x3
             Conv2d(512, 512, kernel_size=3, stride=1, padding=0), #6x3

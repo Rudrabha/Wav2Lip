76ec41e0bfc07839e02b889a0bc9efbd2dc508a2
Eddy Ma <ocean_ltd@hotmail.com>
2024-09-02 01:29:33 -0700

    change param

--- hparams.py
+++ hparams.py
@@ -76,12 +76,12 @@ hparams = HParams(
 	img_size=192,
 	fps=25,
 	
-	batch_size=8,
+	batch_size=11,
 	initial_learning_rate=1e-4,
 	nepochs=200000000000000000,  ### ctrl + c, stop whenever eval loss is consistently greater than train loss for ~10 epochs
 	num_workers=16,
 	checkpoint_interval=2000,
-	eval_interval=2000,
+	eval_interval=5000,
   save_optimizer_state=True,
 
   syncnet_wt=0.0, # is initially zero, will be set automatically to 0.03 later. Leads to faster convergence. 
